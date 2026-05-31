7149cbdae3a4c03e4d5e8efc6c180c0487093908
Aryanhp3m <aryankumarhp3m@gmail.com>
2026-06-01 03:52:13 +0530

    day 1 project setup

---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+function Header() {
+    const isLoggedIn = false;
+    const appName = 'Movie App';
+  
+    return (
+      <>
+        <header className="header">
+          <h1>{appName}</h1>
+  
+          <nav>
+            <a href="/">Home</a>
+            <a href="/movies">Movies</a>
+          </nav>
+  
+          <input type="text" placeholder="Search movies..." />
+  
+          <p>2 + 2 = {2 + 2}</p>
+  
+          <span>{isLoggedIn ? 'Welcome!' : 'Login'}</span>
+        </header>
+      </>
+    );
+  }
+  
+  export default Header;
